65ae4d237816e17c152d9fa67ceb449372ff851b
Tim <microwavehead@gmail.com>
2023-05-08 21:47:19 +0100

    Update

---
+++ assets/AboutView-3e6f3a95.js
@@ -0,0 +1,1 @@
+import{_ as t,o,j as s,k as e}from"./index-0421924c.js";const c={},_={class:"about"},a=e("h1",null,"About",-1),n=e("p",null," A very simple heat pump calculator site that I decided to create to help people decide what size they needed to get ",-1),d=[a,n];function i(l,r){return o(),s("div",_,d)}const p=t(c,[["render",i]]);export{p as default};
